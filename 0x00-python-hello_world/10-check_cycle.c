9ed3fd5c7f96eedb9dd6179fb382850d86a90765
wisdomwest <harmonykoome@gmail.com>
2023-08-07 12:50:33 -0700

    task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,4 +8,17 @@
 
 int check_cycle(listint_t *list)
 {
+	listint_t *s = list;
+	listint_t *f = list;
+
+	while (s && f && f->next)
+	{
+		s = s->next;
+		f = f->next;
+
+		if (s == f)
+			return (1);
+	}
+
+	return (0);
 }

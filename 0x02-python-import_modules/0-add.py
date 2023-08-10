00249d215eda03341e8d8c6e5abbe8bf5613ea6b
wisdomwest <harmonykoome@gmail.com>
2023-08-10 08:32:16 -0700

    confused

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-module = __import__("add_0")
+from add_0 import add
 
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, module.add(a, b)))
+print("{} + {} = {}".format(a, b, add(a,b)))

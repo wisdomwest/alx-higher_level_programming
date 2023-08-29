ad3c8e691dde4897f960ef202bb30a65c5b19ae4
wisdomwest <harmonykoome@gmail.com>
2023-08-29 07:24:05 -0700

    okay i dont know

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 """defines class sqaure"""
 
+
 class Square:
-    """
-    This class defines a square.
-
-    Attributes:
-        No attributes for empty class.
-
-    Methods:
-        No methods for an empty class.
-    """
+    """empty class square"""
     pass

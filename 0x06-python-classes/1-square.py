a402362ba309a12047d9fbfbe16d08b85cdee7bb
wisdomwest <harmonykoome@gmail.com>
2023-08-29 07:30:31 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This class defines a sqaure"""
+
+
+class Sqaure:
+    """
+    This defines a sqaure
+
+    Attributes:
+        __size (int): size of side of square
+    """
+    def __init__(self, size):
+        """
+        Initializes an instance of sqaure class.
+
+        Args:
+            size (int): size of sqaure
+        Returns: None
+        """
+        self.__size = size

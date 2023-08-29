d2bc498661d586375f1bb4ea5ab796c60c176e62
wisdomwest <harmonykoome@gmail.com>
2023-08-29 06:50:14 -0700

    task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    This class defines a square.
+
+    Attributes:
+        No attributes for empty class.
+
+    Methods:
+        No methods for an empty class.
+    """
+    pass

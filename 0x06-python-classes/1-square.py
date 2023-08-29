6f14c6e7e51513bf1282f7c09439f63cbd41e30c
wisdomwest <harmonykoome@gmail.com>
2023-08-29 07:35:19 -0700

    comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""This class defines a sqaure"""
+"""Square class defination"""
 
 
 class Sqaure:
 
-    """This defines a sqaure
+    """Represents a sqaure
 
     Attributes:
         __size (int): size of side of square
+
     """
 
     def __init__(self, size):
 
-        """Initializes an instance of sqaure class.
+        """Initializes a sqaure
 
         Args:
             size (int): size of sqaure
+
         Returns: None
+
         """
 
         self.__size = size

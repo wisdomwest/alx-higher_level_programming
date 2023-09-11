a6e0e220581288e0d42956a6977910a0eb23c577
wisdomwest <harmonykoome@gmail.com>
2023-09-11 12:17:27 -0700

    task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""Module contains the class BaseGeometry"""
+"""Module contains class BaseGeometry and subclass Rectangle"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry:
-    """public instance method area"""
-    def area(self):
-        """raises an exception"""
-        raise Exception("area() is not implemented")
+class Rectangle(BaseGeometry):
+    """rep of rectangle"""
+    def __init__(self, width, height):
+        """instantiation of rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
 
-    def integer_validator(self, name, value):
-        """validate value is int or greater than 0"""
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+     def area(self):
+        """returns area of rectangle"""
+        return self.__width * self.__height
 
-
+     def __str__(self):
+         """informal string of rectangle"""
+         return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)

a1d93788cbd3ec615e43ed6f9c50d698ce29516e
wisdomwest <harmonykoome@gmail.com>
2023-09-11 11:56:59 -0700

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module contains inherits_from function"""
+
+
+def inherits_from(obj, a_class):
+    """returns true if subclass of a_class, otherwise false"""
+    return(issubclass(type(obj), a_class) and type(obj) != a_class)

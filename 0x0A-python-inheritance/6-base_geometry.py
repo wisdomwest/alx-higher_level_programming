41cdc8a16210293607664c3318b62ee60eb7ab78
wisdomwest <harmonykoome@gmail.com>
2023-09-11 12:05:47 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module contains the class BaseGeometry"""
+
+
+class BaseGeometry:
+     """public instance method area"""
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")

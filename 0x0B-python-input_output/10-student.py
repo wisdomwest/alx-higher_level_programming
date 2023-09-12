215c11ab2af8c3978f0573b5929265beda73130f
wisdomwest <harmonykoome@gmail.com>
2023-09-12 11:21:04 -0700

    i am confused

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module contains clas Student"""
+
+
+class Student:
+    """Representation of class student"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns a dictionary representation of class Student """
+        if isinstance(attrs, list) and all(isinstance(ele, str) for ele in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+
+        return self.__dict__

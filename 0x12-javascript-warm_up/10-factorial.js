d8391df1c9838e9c0894c3ab4aa6270fc2b2af76
wisdomwest <harmonykoome@gmail.com>
2023-11-13 14:38:33 -0800

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+function factorial (x) {
+  if (isNaN(x) || x === 0) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
+  }
+}
+
+console.log(factorial(Number(process.argv[2])));

6df0ec7289c1f27cdd2ce53e78a573dfd6722e04
wisdomwest <harmonykoome@gmail.com>
2023-11-13 13:26:52 -0800

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    let row = '';
+    for (let k = 0; k < x; k++)
+      row += 'X';
+    console.log(row);
+  }
+}

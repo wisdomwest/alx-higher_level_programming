93f4dfe54a02d47632fef91f49cd7e9592f6cdbb
wisdomwest <harmonykoome@gmail.com>
2023-11-13 12:32:41 -0800

    single qoutes

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const words = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const words = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < words.length; i++) {
   console.log(words[i]);

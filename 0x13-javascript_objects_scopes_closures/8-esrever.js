ebbb2e8bc6f1e8204148bb6deecec72d5d663b1d
wisdomwest <harmonykoome@gmail.com>
2023-11-14 11:02:53 -0800

    task 7 and 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+exports.esrever = function (list) {
+  const rList = [];
+  let i = list.length - 1;
+  while (i >= 0) {
+    rList.push(list[i]);
+    i -= 1;
+  }
+
+  return rList;
+}

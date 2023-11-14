fc53c26e8918a7ced8bc4cd8d55a50d98ea97e20
wisdomwest <harmonykoome@gmail.com>
2023-11-14 10:55:56 -0800

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const Rectangle = require('./5-square.js');
+
+class Square extends Rectangle {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      let i = 0;
+      for (i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;

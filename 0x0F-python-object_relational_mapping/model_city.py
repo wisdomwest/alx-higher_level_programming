b6122fe6a5e9df1261af09de7ecae5fcda7b45b4
wisdomwest <harmonykoome@gmail.com>
2023-11-16 05:54:56 -0800

    mandatory task

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class defination of city"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """class rep of city table"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)

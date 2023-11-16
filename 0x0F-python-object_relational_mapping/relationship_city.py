c4ed7807f840645462650befbdeb97211294c837
wisdomwest <harmonykoome@gmail.com>
2023-11-16 06:15:24 -0800

    two optional tasks

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class defination of city"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base
+
+class City(Base):
+    """class rep of city table"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)                                                                 
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""class defination of state"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+
+Base = declarative_base()
+
+
+class State(Base):
+    """class rep of states table"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship(
+        "City",
+        cascade="all, delete-orphan",
+        backref=backref("state", cascade="all"),
+        single_parent=True)

c89435f440b09032c72ec99697e072ca4f62d454
wisdomwest <harmonykoome@gmail.com>
2023-12-01 06:41:46 -0800

    easy api lists urls available

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""""get commits and names"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    data = r.json()
+    for i in range(10):
+        print("{}: {}".format(
+                data[i].get("sha"),
+                data[i].get("commit").get("author").get("name")))

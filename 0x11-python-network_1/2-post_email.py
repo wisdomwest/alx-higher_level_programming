a85aec870c98245f75bd04e856b9746a60824e01
wisdomwest <harmonykoome@gmail.com>
2023-12-01 04:04:17 -0800

    test on diffrent sandbox

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""send post req with email"""
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as res:
+        page = res.read().decode("utf-8")
+        print(page)

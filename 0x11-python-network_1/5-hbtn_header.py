e5a8fbb6545511abeef57cf2e7abab19bb66d3ea
wisdomwest <harmonykoome@gmail.com>
2023-12-01 05:32:21 -0800

    task 5 easier than urllib

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""get header value"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    header = r.headers.get("X-Request-Id")
+    print(header)

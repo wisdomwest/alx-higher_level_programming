0cf3022b4e393f2731af95d6aed96cf1d92273db
wisdomwest <harmonykoome@gmail.com>
2023-12-01 03:45:58 -0800

    used getheader

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""display value of x-request-id"""
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        header = res.getheader("X-Request-Id")
+        print(header)

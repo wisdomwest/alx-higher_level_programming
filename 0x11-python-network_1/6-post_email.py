2509929f466061d773a0299d0a534dfa74bd940d
wisdomwest <harmonykoome@gmail.com>
2023-12-01 05:50:45 -0800

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""send email"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email ={"email": sys.argv[2]}
+    r = requests.post(url, data=email)
+    print(r.text)

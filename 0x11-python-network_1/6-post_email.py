3142928b62eb13f1915103f876dc248cbf0136ab
wisdomwest <harmonykoome@gmail.com>
2023-12-01 05:51:30 -0800

    pycode

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,6 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email ={"email": sys.argv[2]}
+    email = {"email": sys.argv[2]}
     r = requests.post(url, data=email)
     print(r.text)

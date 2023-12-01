d0cfb2e09576be76b5d190fec7a5ff9a73886d0b
wisdomwest <harmonykoome@gmail.com>
2023-12-01 03:52:19 -0800

    confused

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,5 +6,6 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         print(res.headers.get("X-Request-Id"))

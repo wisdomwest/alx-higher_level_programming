0a4c39e96ca4578fe8e7e6553a97aba625852bcb
wisdomwest <harmonykoome@gmail.com>
2023-12-01 06:48:30 -0800

    h

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     data = r.json()
     for i in range(10):
         print("{}: {}".format(
-                data[i].get("sha"),
-                data[i].get("commit").get("author").get("name")))
+            data[i].get("sha"),
+            data[i].get("commit").get("author").get("name")))

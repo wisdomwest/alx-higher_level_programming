2a9f24a24bb4699ca397b4992d878f9ffa0f9061
wisdomwest <harmonykoome@gmail.com>
2023-12-01 06:12:44 -0800

    so apis work like this

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""search in jsson"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+
+    search = {"q": letter}
+    r = requests.post("http://0.0.0.0:5000/search_user", data=search)
+    try:
+        res = r.json()
+        if res == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(res.get("id"), res.get("name")))
+    except ValueError:
+        print("Not a valid JSON")

b4c729e3a3c179a1e2ababede283452536b4acb2
wisdomwest <harmonykoome@gmail.com>
2024-01-03 14:20:49 +0300

    task 2 should work not certain about some errors

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const request = require('request');
+try {
+  request.get(process.argv[2]).on('response', (response, err) => {
+    if (err) {
+      console.error(err);
+    }
+    console.log(`code: ${response.statusCode}`);
+  });
+} catch (err) {
+  console.error(err);
+}

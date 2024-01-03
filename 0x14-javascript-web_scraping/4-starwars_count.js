0e19d70590d4d7a5f05f71254fe4ed97d8bb4784
wisdomwest <harmonykoome@gmail.com>
2024-01-03 15:38:02 +0300

    i was confused for a minute but got a hang of it defaulted to use reduce function

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+try {
+  request(url, (err, response, body) => {
+    if (err) {
+      console.error(err);
+    }
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, film) => {
+      if (film.characters.find((character) => character.endsWith('/18/'))) {
+        return count + 1;
+      } else {
+        return count;
+      }
+    }, 0));
+  });
+} catch (err) {
+  console.error(err);
+}

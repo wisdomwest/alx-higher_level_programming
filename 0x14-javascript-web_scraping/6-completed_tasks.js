e2dfec2357033dfb11aa12bde347c1f82c8d3cfe
wisdomwest <harmonykoome@gmail.com>
2024-01-03 17:04:02 +0300

    this wont work

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+const request = require('request');
+
+try {
+  request(process.argv[2], (err, response, body) => {
+    if (err) {
+      console.error(err);
+    }
+
+    const todos_data = JSON.parse(body);
+    const completed = {};
+    todos_data.forEach((todo) => {
+      if (todo.completed && !completed[todo.userId]) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
+      }
+    });
+
+    console.log(completed);
+  });
+} catch (err) {
+  console.error(err);
+}
